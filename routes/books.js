51d9684e5a120f85f57ae6203a2715ef2649d87d
John Garcia <johngarciadev@gmail.com>
2022-07-08 14:16:12 -0700

    Added async wrapper for routes

--- routes/books.js
+++ routes/books.js
@@ -2,13 +2,24 @@ var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
 
+// Async handler function that wraps each route
+function asyncHandler(callBack) {
+    return async (req, res, next) => {
+        try {
+            await callBack(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    }
+};
+
 
 
 /** ROUTES **/
 
 // Get books
 // Gets the full list of books
-router.get('/', async (req, res) => {
+router.get('/', asyncHandler( async (req, res) => {
     // Get books data
     const attributes = ['id', 'title', 'author', 'genre', 'year'];
     const books = await Book.findAll( { attributes: attributes, order: [['createdAt', 'DESC']] });
@@ -19,12 +30,12 @@ router.get('/', async (req, res) => {
         .map( attribute => attribute.charAt(0).toUpperCase() + attribute.slice(1));
     
     res.render('books/books', { columnNames, books, title: 'Books' });
-});
+}));
 
 // Get books/new
 // The create new book form
-router.get('/new', async (req, res) => {
+router.get('/new', asyncHandler( async (req, res) => {
     res.render('books/new-book', { book: {}, title: 'New Book' });
-});
+}));
 
 module.exports = router;
